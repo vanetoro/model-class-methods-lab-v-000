6f3a0f1746d602f2a3d1c7823d9ea0c86d675ef2
Vanessa Toro <vanetoro84@gmail.com>
2018-08-27 17:07:22 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -15,8 +15,8 @@ class Captain < ActiveRecord::Base
   end
 
    def self.talented_seafarers
-     self.where('id in (?)',  
-     self.sailors.pluck(:id) & 
+     self.where('id in (?)',
+     self.sailors.pluck(:id) &
      self.motorboaters.pluck(:id))
    end
   end
